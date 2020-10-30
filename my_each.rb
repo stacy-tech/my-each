927fec1eb6254a1b10495e6072264e46a4a10251
stacy <stacydn23@gmail.com>
2020-10-29 23:33:44 -0400

    commit tree 5d1d67bd62328dfaa049321080101586c412f03d

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)# put argument(s) here
+  if block_given?
+ i = 0 
+
+ while i < array.length
+  yield (array[i])
+  i = i + 1
+ end 
+ array
+else
+   "Hey! No block was given!"
+end 
 end
+
+[1, 2, 3, 4]
+
+my_each([1, 2, 3, 4]) do |index|
+   "num, #{index}"
+end 
+  
